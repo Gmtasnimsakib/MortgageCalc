f9b78c94f8f5e0bf2e3824047612c8d5e2b94ad9
Sakib <openskysakib@gmail.com>
2024-05-30 23:12:09 +0600

    Refactoring Repetitive Patterns (Mosh Course)

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -3,38 +3,31 @@ import java.util.Scanner;
 
 public class MortgageCalculator {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int principal;
         float annualInterest;
         byte years;
 
-        while (true) {
-            System.out.print("Principal (1K - 1M): ");
-            principal = sc.nextInt();
-            if (principal >= 1_000 && 1_000_000 >= principal)
-                break;
-            System.out.println("Enter a number between 10,000 to 1,000,000.");
-        }
+        principal = (int) readNumber("Principal (1K - 1M): ", 1_000, 1_000_000);
+        annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 30);
+        years = (byte) readNumber("Period (Years): ", 1, 30);
 
-        while (true) {
-            System.out.print("Annual Interest Rate: ");
-            annualInterest = sc.nextFloat();
-            if (annualInterest >= 1 && 30 >= annualInterest)
-                break;
-            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
-        }
-
-        while (true) {
-            System.out.print("Period (Years): ");
-            years = sc.nextByte();
-            if (years >= 1 && years <= 30)
-                break;
-            System.out.println("Enter a value between 1 and 30.");
-        }
         double mortgage = mortgageCalculator(principal, annualInterest, years);
 
-        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
-        sc.close();
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+    }
+
+    public static double readNumber(String promt, int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(promt);
+            value = scanner.nextFloat();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Enter a value greater than " + min + " and less than or equal to " + max + ".");
+        }
+        return value;
     }
 
     public static double mortgageCalculator(int principal,
